66ebcdf3e3e9b8e0b2c5a1ff197c20e0ab294679
Anthony Rurup <tcrurup@gmail.com>
2019-06-03 16:31:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  get '/name' do 
+    "My name is Anthony Rurup"
+  end
   
+  get '/hometown' do 
+    'My hometown is Longmont'
+  end
+  
+  get '/song' do
+    'My favortie song is Honeybee'
+  end
 end
