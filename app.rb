795bdd1fc744bc1bed88757ee4e957dc5412eca4
Anthony Rurup <tcrurup@gmail.com>
2019-06-03 16:34:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is Anthony Rurup"
+  end
+  
+  get '/hometown' do 
+    'My hometown is Longmont'
+  end
+  
+  get '/favorite-song' do
+    'My favorite song is Honeybee'
+  end
 end
